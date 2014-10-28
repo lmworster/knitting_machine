f5c49eb0090b0493f2fbdc5d0662973620fb5958
yashachan <yashachan@gmail.com>
2014-10-27 21:45:43 -0400

    Handles missing config file more gracefully.

--- app/tkapp/Config.py
+++ app/tkapp/Config.py
@@ -2,14 +2,20 @@
 
 class Config:
     def __init__(self):
-        config = ConfigParser.ConfigParser({'simulateEmulator':'False'},
-                                           allow_no_value=True)
 
-        with open('config.cfg') as cfgFile:
+        try:
+            cfgFile = open('config.cfg','rb')
+            config = ConfigParser.ConfigParser({'simulateEmulator':'False'},
+                                               allow_no_value=True)
             config.readfp(cfgFile)
             self.imgdir = config.get('UserPrefs','imgdir')
             self.datFile = config.get('UserPrefs','datFile')
             self.device = config.get('UserPrefs','device')
             self.simulateEmulator = config.get('UserPrefs','simulateEmulator')
+        except (OSError, IOError):
+            self.imgdir = ""
+            self.datFile = ""
+            self.device = ""
+            self.simulateEmulator = False
 
         cfgFile.close()
